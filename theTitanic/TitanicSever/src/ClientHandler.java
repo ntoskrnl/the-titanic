48118a70599357763c56fc9bf0c4ee2825209371
danon.oren <danon.oren@gmail.com>
2011-02-18 14:27:58 +0000

    Added some useful functionality and fixed bugs TODO: 	- Create Client-Server-Client Collaboration 	- Implement authentification 	- Implement UserBD

--- theTitanic/TitanicSever/src/ClientHandler.java
+++ theTitanic/TitanicSever/src/ClientHandler.java
@@ -1,41 +1,78 @@
-
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- *
- * @author danon
+ * Client-Server Collaboration
+ * @author danon [Anton Danshin]
  */
-public class ClientHandler {
-    Socket socket = null;
-    public ClientHandler(Socket s) {
+public class ClientHandler implements Comparable<ClientHandler>{
+    @SuppressWarnings("CallToThreadStartDuringObjectConstruction")
+
+    private Socket socket = null;
+    private static int _id=0;
+    private int id;
+    private ConnectionContainer container = null;
+
+    public ClientHandler(ConnectionContainer container, Socket s) {
         socket = s;
-        new Thread(new ClientHandlerRunnable(s)).start();
+        id=_id++;
+        this.container = container;
+        new Thread(new ClientHandlerRunnable(this)).start();
     }
 
+    public void disconnect(){
+        container.remove(this);
+        System.out.println(container.getCount()+" clients are still here!");
+    }
+
+    public void disconnect(String msg){
+        disconnect();
+        System.out.println(msg);
+    }
+
+    public Socket getSocket(){
+        return socket;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public int compareTo(ClientHandler o) {
+        if(this.getId()>o.getId()) return 1;
+        if(this.getId()<o.getId()) return -1;
+        return 0;
+    }
+    
 }
 
 class ClientHandlerRunnable implements Runnable {
     private Socket socket;
-    private InputStream is;
-    public ClientHandlerRunnable(Socket s) {
-        socket = s;
+    private ClientHandler clientHandler;
+    private BufferedReader br;
+
+    public ClientHandlerRunnable(ClientHandler clientHandler) {
+        this.clientHandler = clientHandler;
+        socket = clientHandler.getSocket();
         try{
-            is = s.getInputStream();
-        } catch (Exception ex) {  
+            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception ex) {
+            clientHandler.disconnect();
         }
     }
 
+    public boolean authorize(){
+        return true;
+    }
+
     public void run(){
-        BufferedReader br = new BufferedReader(new InputStreamReader(is));
         String s;
+        if(!authorize()){
+            clientHandler.disconnect("Authorization problem.");
+            return;
+        }
         while(true){
             try{
                 s = br.readLine();
@@ -45,7 +82,7 @@ class ClientHandlerRunnable implements Runnable {
                 break;
             }
             if(s.equals("exit")){
-                System.out.println("Client set terminal command.");
+                System.out.println("Client sent terminal command.");
                 try{
                     socket.close();
                 } catch(Exception ex){}
@@ -53,5 +90,6 @@ class ClientHandlerRunnable implements Runnable {
             }
             System.out.println(s);
         }
+        clientHandler.disconnect();
     }
 }
