543364bc789cc010b75a295cc4ca340d0e134e5e
danon.oren@gmail.com <danon.oren@gmail.com@e0c62b43-ddc9-1b93-bfd1-2ec8dfe321c9>
2011-02-18 14:27:58 +0000

    Added some useful functionality and fixed bugs TODO: 	- Create Client-Server-Client Collaboration 	- Implement authentification 	- Implement UserBD

--- trunk/theTitanic/TitanicSever/src/Main.java
+++ trunk/theTitanic/TitanicSever/src/Main.java
@@ -1,27 +1,40 @@
 import java.net.*;
-import java.sql.Time;
-import java.util.Date;
 
 /**
  * TheTitanic Server Side Main Class
  * @author DANON [Anton Danshin]
  */
 public class Main {
+    /**
+     * Contains all currently active connections
+     */
+    static ConnectionContainer connections;
+    /**
+     * ServerSocket to accept incoming connections
+     */
+    static ServerSocket server;
+
+    /**
+     * Default port for incoming connections
+     */
+    static final int DEFAULT_PORT = 10000;
 
     /**
      * Program entry point
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ConnectionContainer connections = new ConnectionContainer();
+        connections = new ConnectionContainer();
         try{
-            ServerSocket server = new ServerSocket(10000);
+            server = new ServerSocket(DEFAULT_PORT);
             while(true){
                 Socket newClient = server.accept();
                 connections.add(newClient);
                 System.out.println(System.currentTimeMillis()+"\tNew client has just connected to the server!");
             }
         }catch(Exception ex){
+            //Error
+            System.out.println("Error!");
             System.err.println(ex);
         }
     }
