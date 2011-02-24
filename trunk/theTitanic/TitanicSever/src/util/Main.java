6fcc6fc5a1939e95cde4d0c5f0e967e42beaa97d
danon.oren@gmail.com <danon.oren@gmail.com@e0c62b43-ddc9-1b93-bfd1-2ec8dfe321c9>
2011-02-24 18:41:30 +0000

    Now the server saves its configuration and restores it at start.

--- trunk/theTitanic/TitanicSever/src/util/Main.java
+++ trunk/theTitanic/TitanicSever/src/util/Main.java
@@ -6,20 +6,33 @@ import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import java.util.prefs.Preferences;
 
 /**
  * TheTitanic Server Side Main Class
  * @author DANON [Anton Danshin]
  */
 public class Main {
-    
+
     private static ServerConfigWindow configWindow = null;
     private static Thread mainServerThread = null;
+    private static Preferences userPreferences = null;
+
+    static ConnectionContainer connections;
+    /**
+     * ServerSocket to accept incoming connections
+     */
+    static ServerSocket server;
+
     /**
      * Program entry point
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        ServerConfiguration.loadPreferences();
+        applyLookAndFeel(ServerConfiguration.lookAndFeel);
+
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 configWindow = new ServerConfigWindow();
@@ -27,6 +40,14 @@ public class Main {
                 configWindow.setVisible(true);
             }
         });
+    }
+
+    public static void applyLookAndFeel(String l){
+        try {
+            UIManager.setLookAndFeel(l);
+        } catch (Exception ex){
+            System.err.println("Could not find "+l);
+        }
     }
 
     public static void startServer(){
@@ -37,6 +58,8 @@ public class Main {
     public static Thread getMainServerThread(){
         return mainServerThread;
     }
+
+
 
 }
 
