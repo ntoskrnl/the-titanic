e2a4f861afc4ca34daf005fd921d38e931455301
agilmullin@gmail.com <agilmullin@gmail.com@e0c62b43-ddc9-1b93-bfd1-2ec8dfe321c9>
2011-03-17 12:06:35 +0000

    Coordinates correction is added.

--- trunk/theTitanic/TitanicGameClient/src/client/util/SimplePhysicsConst.java
+++ trunk/theTitanic/TitanicGameClient/src/client/util/SimplePhysicsConst.java
@@ -1,4 +1,4 @@
-package client.util;
+package ballsimpact;
 
 /**
  *
@@ -7,5 +7,8 @@ package client.util;
 /* Постоянные физического движка: все в одном месте, чтобы
                 править было легче. */
 public class SimplePhysicsConst {
-
+   static double N = 0.0; // Коэффицент трения.
+   static int PhysicsModel = 1; //Модель физики: 1 или 2.
+   static int CorectionEnable = 1; //Использование коррекции.
+   static int CorrectionPenalty = 10;
 }
