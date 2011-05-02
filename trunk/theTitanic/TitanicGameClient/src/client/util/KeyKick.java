9da67fc599798a0b1443bb8490f6de7e943397db
out31@rambler.ru <out31@rambler.ru@e0c62b43-ddc9-1b93-bfd1-2ec8dfe321c9>
2011-05-02 06:58:43 +0000

    Add animation of Key and Strengh of it

---
+++ trunk/theTitanic/TitanicGameClient/src/client/util/KeyKick.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package client.util;
+
+/**
+ *
+ * @author 7
+ */
+
+import javax.media.j3d.*;
+import javax.vecmath.*;
+import titanic.basic.Game;
+
+
+public class KeyKick extends Thread {
+
+    private Game game;
+    private TransformGroup tr;
+    private double A;
+    private Transform3D trans;
+    private double time=0;
+    private float Str=0;
+     
+    public KeyKick(TransformGroup tr, double A,float STR, Game game) {
+    this.game = game;
+    this.tr = tr;
+    this.A = A;
+    this.Str = STR;
+    trans = new Transform3D();
+
+    }
+
+    @Override
+    public void run(){
+
+
+       while((time-Math.PI)<0){
+        try{
+        Thread.currentThread().sleep((int)(8*(1.2-Str)));
+        time+=0.01;
+       
+       if(time-Math.PI/2 < 0) trans.setTranslation(new Vector3d(0.0, -0.5*A*Math.sin((time)), 0));
+       else trans.setTranslation(new Vector3d(0.0, -0.5*A*Math.sin((time)), 0));
+
+
+        tr.setTransform(trans);
+
+            }
+    catch(Exception ex){
+        System.err.print(ex);
+    }
+        }
+       game.getBilliardKey().setPower(Str);
+    }
+
+}
