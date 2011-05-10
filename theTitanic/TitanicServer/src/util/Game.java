0d097dfb6b40b475f856c6fae90c7264e586a9fb
danon.oren <danon.oren@gmail.com>
2011-05-10 00:56:57 +0000

    Server assigns status of player

--- theTitanic/TitanicServer/src/util/Game.java
+++ theTitanic/TitanicServer/src/util/Game.java
@@ -20,6 +20,10 @@ public class Game {
         balls = new Ball[16];
         for(int i=0;i<16;i++)
             balls[i]= new Ball(i + " 0 0 0 0 true");
+        player1.setStatus(Game.S_BALL_SELECT);
+        player1.setPlayNext(false);
+        player2.setStatus(Game.S_WAIT_RIVAL);
+        player2.setPlayNext(true);
     }
     
     public String getID(){
@@ -51,8 +55,51 @@ public class Game {
         }
         return false;
     }
+    
+    public void resolveTurn(){
+        if(whoPlays.equals(player1)){
+            whoPlays = player2;
+            player2.setStatus(S_BALL_SELECT);
+            player1.setStatus(S_WAIT_RIVAL);
+        } else {
+            whoPlays = player1;
+            player1.setStatus(S_BALL_SELECT);
+            player2.setStatus(S_WAIT_RIVAL);
+        }
+    }
 
     Ball[] getBalls() {
         return balls;
     }
+        
+        /**
+     * Waiting for user to make next impact. (no physics)
+     */
+    public static final int S_BALL_SELECT = 1;
+    /**
+     * Now the balls are moving (physics is active)
+     */
+    public static final int S_MOVING = 2;
+    /**
+     * Animation of the key to simulate a hit.
+     */
+    public static final int S_MAKE_HIT = 3;
+    /**
+     * The game is over. It's time for the final verdict.
+     */
+    public static final int S_FINISH = 4;
+    /**
+     * Coffee break or something like that (no physics).
+     */
+    public static final int S_PAUSE = 5;
+    /**
+     * Wait for rival to make a hit
+     */
+    public static final int S_WAIT_RIVAL = 6;
+    /**
+     * Synchronisation
+     */
+    public static final int S_SYNC = 7;
+
+    public static final int S_NONE = 0;
 }
